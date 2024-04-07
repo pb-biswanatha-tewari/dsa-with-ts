c3ab914c3be95d5211cfdc5b27d82cf4bb6d3c2a
biswa <biswanath@geekyants.com>
2024-04-07 22:31:18 +0530

    max-subarray

---
+++ dsa/arrays/maximum-subarray.ts
@@ -0,0 +1,38 @@
+/* 
+question link: https://leetcode.com/problems/maximum-subarray/
+time complexity: O(N) best case
+intution: 1. Brute force: create a prefix sum and loop(j) over the prefix sum array while subtracting arr[i] to find the max sub array
+          2. Kaden's algo: we rest the sum if (sum < 0), edge cases: [array length of 1 & all negative numbered elements]
+*/
+
+function maxSubArray(nums: number[]): number {
+  let sum = 0;
+  let max = -111111;
+
+  if (nums.length === 1) return nums[0];
+
+  // check if all negative
+  let isAllNegative = true;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= 0) {
+      max = Math.max(nums[i], max);
+    } else {
+      isAllNegative = false;
+      break;
+    }
+  }
+
+  if (isAllNegative) return max;
+  else max = -11111111;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    max = Math.max(sum, max);
+
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+
+  return max;
+}
